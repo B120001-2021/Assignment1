f00ed0c5abe694f0846d168d47fd325e64434741
B120001-2021 <s1757117@ed.ac.uk>
2021-10-31 16:42:48 +0000

    v2, with interactive prompt

--- question1to5.sh
+++ question1to5.sh
@@ -1,37 +1,56 @@
-#! /bin/bash
-
-#making a directory for this assignment
+ng a directory for this assignment
 mkdir assignment1
 #working on assignment1 directory for this assignment
 cd assignment1
 
+echo "Hello~ Fastqc report for illumina sequencing results is being generated."
 #use fastqc to check the quality of illumina sequencing results
 #unzip and integrate the 45 RNA-seq files to one file for output one fastqc report
 zcat /localdisk/data/BPSM/AY21/fastq/*.fq.gz > allsample.fq  
 #fastqc with 4 threads
 fastqc -t 4 allsample.fq
+
+#Assessment of the number and quality of the raw sequence
+unzip allsample.fq.zip
+cd allsample.fq
+echo "The number of analyzed reads: `grep "Total Sequences" fastqc_data.txt`"
+echo "The quality of raw RNA-seq: `grep "Sequences flagged as poor quality" fastqc_data.txt`"
+echo "Modules that did not pass FastQC are: `grep -v "PASS" summary.txt | cut -f 1,2`" 
+echo "Modules that passed FastQC are: `grep "PASS" summary.txt | cut -f 1,2`"
+
+cd assignment1
+
+echo "After viewing the fastqc report, please press control & C to quit."
 firefox *.html #there would be a report in html after fastqc and can be oepn with firefox
+
+#interactive prompt for users to choose continue or exist
+while true; do
+    read -p "Are you satisfied with the quality of sequencing? Please type 'y' or 'n':" answer
+    case $answer in
+    	[Yy]* ) break;;
+        [Nn]* ) echo "Bye ~ ~ ~" ; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+rm allsample*
+
+echo "Good choice. Hello again~"
 
 #align the forward and reverse reads with bowtie2
 #extract referencegene for reads alignment
-zcat /localdisk/data/BPSM/AY21/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz > referencegene.fq
-bowtie2-build -f referencegene.fq referencegene #build index and name the index as referencegene
+bowtie2-build -f -q /localdisk/data/BPSM/AY21/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz referencegene #build index and name the index as referencegene
+echo "Index of Tcongo_genome has been built."
 
 #align the forward and reverse reads against reference gene for each sample
 for i in /localdisk/data/BPSM/AY21/fastq/*1.fq.gz #wildcard was used here for extract the name of all fq.gz files that needs to be aligned
 do
 name=$(basename $i _1.fq.gz) #get the name of each variable without suffix
-bowtie2 -x referencegene -1 /localdisk/data/BPSM/AY21/fastq/${name}_1.fq.gz -2 /localdisk/data/BPSM/AY21/fastq/${name}_2.fq.gz|samtools view -b -o ${name}.bam
+bowtie2 -x referencegene -1 /localdisk/data/BPSM/AY21/fastq/${name}_1.fq.gz -2 /localdisk/data/BPSM/AY21/fastq/${name}_2.fq.gz|samtools sort -o ${name}.sort.bam
 done
+echo "Sequence of each sample has been aligned with bowtie2 and sorted by samtools"
 
 rm reference* #remove the indexed referencegene for alignments because we don't need it anymore
-
-#for sort the bam file first to allow following construction of indexed bam file, also makes bedtools run fatser when compare against the reference genotype
-for i in *.bam
-do
-bamname=$(basename $i .bam)
-samtools sort ${bamname}.bam -o ${bamname}.sort.bam
-done
 
 #construct indexed bam file
 for i in *sort.bam
